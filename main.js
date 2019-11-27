af2c1e1e7515353d9252ecd6d6b413f244b198c7
knuchanwoo <39613774+knuchanwoo@users.noreply.github.com>
2019-11-28 02:54:13 +0900

    Add files via upload
    gg

--- main.js
+++ main.js
@@ -5,52 +5,114 @@ var path = require('path');
 var qs = require('querystring');
 var bodyParser = require('body-parser');
 var sanitizeHtml = require('sanitize-html');
-var compression = require('compression')
+var compression = require('compression');
+var joinTemplate = require('./lib/joinTemplate.js');
 var loginTemplate = require('./lib/loginTemplate.js');
-var borderTemplate = require('./lib/borderTemplate.js');
+var circleMainTemplate = require('./lib/circleMainTemplate.js');
 var circleTemplate = require('./lib/circleTemplate.js');
 var homeTemplate = require('./lib/homeTemplate.js');
+var mysql = require('./mysqlDB.js');
+
+mysql.connect();
+
 var helmet = require('helmet');
 var cookieParser = require('cookie-parser');
+
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
-app.get('*', function(request, response, next){
-  fs.readdir('./data', function(error, filelist){
-    request.list = filelist;
-    next();
-  });
+
+console.log("익스프레스 실행");
+
+app.get('/',function(request,response){
+  var html = homeTemplate.html();
+  response.send(html);
 });
 
-app.get('/', function(request, response){
-
-  if(false){  // 쿠키 체크
+app.get('/login', function(request, response){
+  var html = loginTemplate.html();
+  response.send(html);
+  /*if(false){  // 쿠키 체크
     alert("로그인이 필요합니다");
     response.redirect('/');
   }
   var html = homeTemplate.html();
+  response.send(html);*/
+});
+
+app.post('/login_process', function(request,response){
+  var post=request.body;
+  var id=post.ID;
+  var pw=post.PW;
+  
+  var sql="SELECT * FROM user WHERE ID=?";
+  mysql.query(sql,[id],function(err,results){
+    if(err)
+      console.log(err);
+
+    if(!results[0])
+      response.send(`
+      <script type = "text/javascript">alert("없는 아이디 입니다.");
+      location.href='/login';
+      </script>
+      `);
+    else
+    {
+      var user=results[0];
+      if(pw === user.PW){
+        response.cookie('authority',results.AUTHORITY);
+        response.send(`
+            <script type = "text/javascript">alert("로그인 성공");
+            location.href='/';
+            </script>
+            `);
+      }
+      else
+      {
+        response.send(`
+            <script type = "text/javascript">alert("비밀번호가 다릅니다.");
+            location.href='/login';
+            </script>
+            `);
+      }
+    }
+  });
+});
+
+app.get('/join', function(request, response){
+  var html = joinTemplate.html();
   response.send(html);
 });
-app.get('/login_page', function(request, response){
-  var html = loginTemplate.html();
-  response.send(html);
+
+app.post('/create_process',function(request,response){
+  console.log("createprocess");
+  var post = request.body;
+  console.log(post);
+  var userId = post.ID;
+  var userPw = post.password;
+  var userName = post.name;
+  var userAuthority = post.authority;
+  
+    mysql.query('insert into user values(?,?,?,?)', [userId, userPw, userName, userAuthority], function (err, rows, fields) {
+      if (!err) {
+           response.send(`
+           <script type = "text/javascript">alert("회원가입 성공");
+           location.href='/login';
+           </script>
+           `);
+      } else {
+           response.redirect('/join');
+      }
+    });
+    //response.redirect('/login');
+  // fs.writeFile(`./UserData/${ID}`, PW, 'utf8', function(err){
+  //   response.redirect('/login');
+    //response.send(302,{Location:`/?id=${ID}`});
 });
-app.post('/login_process', function(request, response){
-  fs.readdir('./data/loginData', function(error, filelist){
 
 
-  })
-  if(true){     //로그인 맞으면
-    //쿠키 생성
-    response.redirect('/');
 
-  }
-  else{
-    alert("아이디 혹은 비밀번호가 틀렸습니다");
-    response.redirect('/login_page');
-  }
-});
 app.get('/circles', function(request, response){
   console.log(request.query.id);
   var html = circleTemplate.html(request.query.id);
@@ -63,4 +125,4 @@ app.get('/board_page', function(request, response){
 });
 
 
-app.listen(3000);
+app.listen(8000);
